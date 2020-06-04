b614075727049e0de8ac07cad75568b3ec3613ba
unknown <tafadzwatracey@gmail.com>
2020-06-04 22:29:15 +0800

    Started registration page

---
+++ reg_page/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <Login msg="Login"/>
+  </div>
+</template>
+
+<script>
+import Login from './components/Login.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Login
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  margin-top: 60px;
+  width: 100%;
+}
+</style>
---
+++ reg_page/src/components/Login.vue
@@ -0,0 +1,97 @@
+<template>
+  <a-form
+    id="components-form-demo-normal-login"
+    :form="form"
+    class="login-form"
+    @submit="handleSubmit"
+  >
+    <a-form-item>
+      <a-input
+        v-decorator="[
+          'userName',
+          { rules: [{ required: true, message: 'Please input your username!' }] },
+        ]"
+        placeholder="Username"
+      >
+        <a-icon slot="prefix" type="user" style="color: rgba(0,0,0,.25)" />
+      </a-input>
+    </a-form-item>
+    <a-form-item>
+      <a-input
+        v-decorator="[
+          'password',
+          { rules: [{ required: true, message: 'Please input your Password!' }] },
+        ]"
+        type="password"
+        placeholder="Password"
+      >
+        <a-icon slot="prefix" type="lock" style="color: rgba(0,0,0,.25)" />
+      </a-input>
+    </a-form-item>
+    <a-form-item>
+      <a-checkbox
+        v-decorator="[
+          'remember',
+          {
+            valuePropName: 'checked',
+            initialValue: true,
+          },
+        ]"
+      >
+        Remember me
+      </a-checkbox>
+      <a class="login-form-forgot" href="">
+        Forgot password
+      </a>
+      <a-button type="primary" html-type="submit" class="login-form-button">
+        Log in
+      </a-button>
+      Or
+      <a href="">
+        register now!
+      </a>
+    </a-form-item>
+  </a-form>
+
+</template>
+
+<script>
+export default {
+  beforeCreate() {
+    this.form = this.$form.createForm(this, { name: 'normal_login' });
+  },
+  methods: {
+    handleSubmit(e) {
+      e.preventDefault();
+      this.form.validateFields((err, values) => {
+        if (!err) {
+          console.log('Received values of form: ', values);
+        }
+      });
+    },
+  },
+};
+</script>
+<style scoped>
+
+
+#components-form-demo-normal-login .login-form {
+  max-width:100%;
+  
+}
+#components-form-demo-normal-login .login-form-forgot {
+  float: right;
+}
+#components-form-demo-normal-login .login-form-button {
+  width: 96%;
+  background-color: chocolate;
+  margin-left:15%;
+  margin-right:15%;
+  text-align: center;
+  border:1px solid grey;
+  border-radius:10px;
+  padding:7px;
+  font-size: 1em;
+}
+
+</style>
